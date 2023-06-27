8c32d54b738708b739263c58bd1c9e4abfc43b0b
ecoelho03 <ecoelho@contanciainvest.com.br>
2023-06-27 11:08:48 -0300

    Exercicio 8 finalizado,
    Questão: Escreva um programa que informe em que dia da semana cairá uma determinada data. Dica: investigue a API da classe SimpleDateFormat.

---
+++ Lista-exercicios-UFES-master/src/exercicio8/DiaDaSemana.java
@@ -0,0 +1,62 @@
+package exercicio8;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DiaDaSemana {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
+
+        boolean dataValida = false;
+        String dataString;
+
+        while (!dataValida) {
+            System.out.print("Digite uma data (dd/MM/yyyy): ");
+            dataString = scanner.nextLine();
+
+            // Verificar se a data contém letras
+            Pattern pattern = Pattern.compile("[a-zA-Z]");
+            Matcher matcher = pattern.matcher(dataString);
+
+            if (matcher.find()) {
+                System.out.println("Formato de data inválido. A data não deve conter letras.");
+            } else {
+                try {
+                    formatoData.setLenient(false); // Impede a interpretação de datas inválidas, como 31/02/2023
+                    Date data = formatoData.parse(dataString);
+
+                    Calendar calendario = Calendar.getInstance();
+                    calendario.setTime(data);
+
+                    int diaSemana = calendario.get(Calendar.DAY_OF_WEEK);
+                    String nomeDiaSemana = obterNomeDiaSemana(diaSemana);
+
+                    System.out.println("A data " + formatoData.format(data) + " cai em um(a) " + nomeDiaSemana + ".");
+                    dataValida = true;
+                } catch (java.text.ParseException e) {
+                    System.out.println("Formato de data inválido. Use o formato dd/MM/yyyy.");
+                }
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static String obterNomeDiaSemana(int diaSemana) {
+        return switch (diaSemana) {
+            case Calendar.SUNDAY -> "Domingo";
+            case Calendar.MONDAY -> "Segunda-feira";
+            case Calendar.TUESDAY -> "Terça-feira";
+            case Calendar.WEDNESDAY -> "Quarta-feira";
+            case Calendar.THURSDAY -> "Quinta-feira";
+            case Calendar.FRIDAY -> "Sexta-feira";
+            case Calendar.SATURDAY -> "Sábado";
+            default -> "Desconhecido";
+        };
+    }
+}
